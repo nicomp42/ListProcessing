9d5840822ff88bbae75a2b4d7609005287478b1b
nicomp42 <nicholdw@ucmail.uc.edu>
2024-09-16 07:40:21 -0400

    Add project files.

---
+++ ListProcessing/ListProcessingPackage/listProcessing.py
@@ -0,0 +1,17 @@
+# listProcessing.py
+# Bill Nicholson
+# nicholdw@ucmail.uc.edu
+
+def initializeStudents():
+    """
+    Create a dataset of students to work with.
+    @return list: The initialized list. Actually, a list of lists
+    """
+    data = []    # An empty list
+    student = ["Smith", "Bob", "M02635916", "Information Systems"]
+    data.append(student)
+    student = ["Miller", "Mary", "M02635922", "Information Systems"]
+    data.append(student)
+    student = ["Burlew", "Cecelia", "M02635987", "BANA"]
+    data.append(student)
+    return data
---
+++ ListProcessing/mainPackage/main.py
@@ -0,0 +1,13 @@
+#main.py
+# Bill Nicholson
+# nicholdw@ucmail.uc.edu
+
+from ListProcessingPackage.listProcessing import *
+
+if __name__ == "__main__":
+    students = initializeStudents()
+    print(students)
+    # Print the data type of students
+    # Print the elements of the list one-at-a-time with a for/in loop
+    # Add an element to the list (another student)
+    
